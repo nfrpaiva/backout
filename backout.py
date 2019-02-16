c8c65d11ec10de4da8a8e56d4d444ed3dd8606b5
Nilton Paiva <nfrpaiva@gmail.com>
2019-02-15 23:11:20 -0200

    xlsx

--- backout.py
+++ backout.py
@@ -8,11 +8,15 @@ ws.title = 'Backout'
 
 response = requests.get('https://jsonplaceholder.typicode.com/users')
 cabecalho = ['id', 'nome', 'email', 'telefone']
+for value in cabecalho:
+    ws.cell(row=1, column=cabecalho.index(value) + 1, value=value)
+columns = len(cabecalho)
 if response.status_code == 200:
     list = response.json()
-    with open('arquivo.csv', 'w+') as file:
-        file.write(';'.join(cabecalho) + '\n')
-        for i in list:
-            linha = ';'.join(
-                (str(i['id']), i['name'], i['email'], i['phone'])) + '\n'
-            file.write(linha)
+    rows = len(list)
+    for i, item in enumerate(list, 1):
+        ws.cell(row=i, column=1, value=item['id'])
+        ws.cell(row=i, column=2, value=item['name'])
+        ws.cell(row=i, column=3, value=item['email'])
+        ws.cell(row=i, column=4, value=item['phone'])
+    wb.save('backout.xlsx')
